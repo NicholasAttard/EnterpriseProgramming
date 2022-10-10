0157cd30fc906071e33a2a82034ce2d3a65e5a4d
Nicholas Attard <nicholas.attard.g53416@mcast.edu.mt>
2022-10-10 08:28:31 +0200

    First Commit, preparing architecture

---
+++ Domain/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    //Domain: will be a project referenced by all the other layers and will hold the data containers i.e models
+    public class Class1
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public int CategoryId { get; set; }
+        public double Price { get; set; }
+        public String Description { get; set; }
+        public String PhotoPath { get; set; }
+    }
+}
