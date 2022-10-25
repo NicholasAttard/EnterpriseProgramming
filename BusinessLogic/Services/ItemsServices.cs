2fb6aa6c16bb7bb4f0a907fac9867f20a3ea4e10
Nicholas Attard <nicholas.attard.g53416@mcast.edu.mt>
2022-10-25 13:55:45 +0200

    lesson 25/10/2022

--- BusinessLogic/Services/ItemsServices.cs
+++ BusinessLogic/Services/ItemsServices.cs
@@ -37,6 +37,7 @@ namespace BusinessLogic.Services
         public void DeleteItem(int id)
         {
         }
+        public void CheckOut() { }
 
         //It is not recommended to use the domain.Models as a return type
         //In other words do not use the classes that model the database to transfer data into the presentation layer
@@ -53,6 +54,21 @@ namespace BusinessLogic.Services
                            Price = i.Price
                        };
             return list;
-            }
+        }
+
+        public IQueryable<ItemViewModel> Search(string Keyword) 
+        {
+            return GetItems().Where(x => x.Name.Contains(Keyword));
+        }
+
+        public IQueryable<ItemViewModel> Search(string Keyword, double minPrice, double maxPrice) 
+        {
+            return Search(Keyword).Where(x => x.Price >= minPrice && x.Price <= maxPrice);
+        }
+
+        public ItemViewModel GetItem(int id) 
+        {
+            return GetItems().SingleOrDefault(x => x.Id == id);
         }
     }
+}
